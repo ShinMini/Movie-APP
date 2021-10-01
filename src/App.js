ab82d959b1a78012d43b52f1d97018b6808ac209
ShinMini <gusals121234@gmail.com>
2021-10-01 17:12:55 +0900

    왜 푸쉬 안되냐

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React from "react";
 
 class App extends React.Component {
+    state = {
+        count:0,
+    };
+
+    add =() =>{
+        this.setState(current => ({
+            count: current.count + 1,
+        }));
+    }
+    minus = () => {
+        this.setState(current => ({
+            count: current.count - 1,
+        }));
+    }
+
     render(){
-        return <h1>I'm a class component!</h1>;
+        return(
+            <div>
+                <h1>the number is: {this.state.count}</h1>
+                <button onClick={this.add}>Add</button>
+                <button onClick={this.minus}>Minus</button>
+            </div>
+    );
     }
 }
 
