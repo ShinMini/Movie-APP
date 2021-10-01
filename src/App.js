483e2ab5e04a4653412d0a44004c01b800d09b02
ShinMini <gusals121234@gmail.com>
2021-10-01 11:28:05 +0900

    learn about component using arrow function!

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import React from "react";
 import './App.css';
 
-function PropsExample(props){
-    return <h1>I really don't like {props.food}!!!</h1>
+function LikeFoods({name}){
+    return <h1>I really don't like {name}. </h1>;
 }
+
+const foodHate = [
+    {
+        name: 'kimchi',
+        grade: 3
+    },
+    {
+        name: 'patato',
+        grade: 2
+    },
+    {
+        name: 'tomato',
+        grade: 1
+    }
+];
+
+
 function App() {
   return (
       <div className={"container"}>
         <h1>
           Hello, my first React project :)
         </h1>
-          <PropsExample food={"potato"}/>
-          <PropsExample food={"tomato"}/>
-          <PropsExample food={"everything"}/>
+          {
+              foodHate.map ( hates => <LikeFoods name={hates.name}/> )
+          }
       </div>
   );
 }
