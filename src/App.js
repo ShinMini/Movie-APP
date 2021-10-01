cb2a1e656bb038b1cad348415e2659e3e870943f
ShinMini <gusals121234@gmail.com>
2021-10-01 16:48:57 +0900

    Update App.js
    class and component practice example file

--- src/App.js
+++ src/App.js
@@ -1,37 +1,9 @@
 import React from "react";
-import './App.css';
 
-function LikeFoods({name}){
-    return <h1>I really don't like {name}. </h1>;
-}
-
-const foodHate = [
-    {
-        name: 'kimchi',
-        grade: 3
-    },
-    {
-        name: 'patato',
-        grade: 2
-    },
-    {
-        name: 'tomato',
-        grade: 1
+class App extends React.Component {
+    render(){
+        return <h1>I'm a class component!</h1>;
     }
-];
-
-
-function App() {
-  return (
-      <div className={"container"}>
-        <h1>
-          Hello, my first React project :)
-        </h1>
-          {
-              foodHate.map ( hates => <LikeFoods name={hates.name}/> )
-          }
-      </div>
-  );
 }
 
 export default App;
