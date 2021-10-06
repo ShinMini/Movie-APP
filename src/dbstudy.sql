7d88846756695e694277ec31b6a9318cb1219bf1
ShinMini <gusals121234@gmail.com>
2021-10-06 14:40:27 +0900

    create db leraned recode
    that db from collage lecture progressed

---
+++ src/dbstudy.sql
@@ -0,0 +1,9 @@
+INSERT INTO TEST1 (a, b) values('한강', 1);
+INSERT INTO TEST1 (a, b) values('낙동강', 2);
+INSERT INTO TEST1 (a, b) values('금강', 3;
+INSERT INTO TEST1 (a, b) values('영산강', 4);
+
+INSERT INTO TEST2 (b, c) values('1', '백두산');
+INSERT INTO TEST2 (b, c) values('2', '한라산');
+INSERT INTO TEST2 (b, c) values('5', '지리산');
+INSERT INTO TEST2 (b, c) values('6', '설악산');
