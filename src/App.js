80ad1c62fe3329f2c55df1cb71861eb6bc5233f2
ShinMini <gusals121234@gmail.com>
2021-10-07 17:09:24 +0900

    update component getMoives
    add a function that received movie data information values are classified separately and stored in movies object
    
    and using this.setState component to print message in movie's object after loading

--- src/App.js
+++ src/App.js
@@ -11,11 +11,18 @@ class App extends React.Component {
     };
 
     render(){
-        const {isLoading} =this.state;
-        return (<div>{isLoading ? 'Loading...' : 'We are ready'}</div>);
+        const {isLoading, movies} =this.state;
+        return (<div>{isLoading ? 'Loading...' : movies.map()}</div>);
     };
     getMovies = async () => {
-        const movies = await axios.get("https://yts.mx/api/v2/list_movies_details.json");
+        const {     // 받아온 객체 내 영화 데이터 정보값을 따로 분류해서 movies 에 저장
+            data: {
+                data: { movies }, // const movies 객체 생성
+            },
+        } = await axios.get("https://yts.mx/api/v2/list_movies.json?sort_by=rating");   // sort_by를 통해 별점 순서대로 정렬
+        // 객체의 키와 대입할 변수의 이름이 같다면 코드를 축약 가능함 => {movies: movies} == {movies}
+        this.setState({movies: movies, isLoading: false});    // state 안에 있는 배열객체 movies 안에 getMovies()를 통해 저장된 movies 객체
+        // movies 데이터를 가져온뒤 isLoading 값을 false 로 바꿔 로딩을 끝내줌
     };
     componentDidMount() {
         this.getMovies();
