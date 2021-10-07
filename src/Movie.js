2f51fb91671ca51f05e448ae1d3af24286080ca6
ShinMini <gusals121234@gmail.com>
2021-10-07 17:05:56 +0900

    create movie.js file
    movie.js processes and sends data

---
+++ src/Movie.js
@@ -0,0 +1,16 @@
+import React from "react";
+import PropTypes from 'prop-types';
+
+function Moive({id, title, year, summary, poster}) {
+    return <h4>{title}</h4>;
+}
+
+Movie.propTypes = {
+    id: PropTypes.number.isRequired,    // 영화 id
+    year: PropTypes.number.isRequired,  // 영화 발매 년도
+    title: PropTypes.string.isRequired,     // 영화 이름(string)
+    summary: PropTypes.string.isRequired,   // summary(string)
+    poster: PropTypes.string.isRequired,    // 이미지 주소(string)가 저장   => 영화 포스터(movie poster)
+};
+
+export default Movie;
